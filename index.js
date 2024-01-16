4ab00e78cc2cf496061fafbd07d24f09ebccea72
rushikeshhirve <rushikeshhirve09@gmail.com>
2024-01-16 18:13:49 +0530

    code change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const pg = require("pg");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs")
 // const path = require("path");
 // const { dirname, fileURLToPath } = require("url");
 const dotenv = require('dotenv');
